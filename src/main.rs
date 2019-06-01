fa4ed479b0e4f85604f68ecc814e63bd3f3a14d3
Vesa Laakso <laakso.vesa@gmail.com>
2019-06-01 21:50:27 +0300

    Keep track of what we know

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,13 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-enum GuessDir {
-    LESS,
-    GREATER,
-    UNKNOWN,
-}
-use GuessDir::*;
-
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut guesses = 0;
-    let mut previous_guess_direction = UNKNOWN;
+    let mut largest_guess: Option<u32> = None;
+    let mut smallest_guess: Option<u32> = None;
 
     loop {
         println!("Please input your guess number {}.", guesses + 1);
@@ -37,26 +31,38 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => {
                 println!("Too small!");
-                match previous_guess_direction {
-                    LESS => println!("Still going smaller, eh?"),
-                    GREATER => println!("Ok nice, now you know some boundaries"),
-                    _ => println!("Try again :)"),
-                };
-                previous_guess_direction = LESS;
+                if smallest_guess.is_none() {
+                    smallest_guess = Some(guess);
+                } else {
+                    if smallest_guess.expect("There was no prev guess") < guess {
+                        smallest_guess = Some(guess);
+                    } else {
+                        println!("Why did you waste your guess for nothing? That's stupid.");
+                    }
+                }
             }
             Ordering::Greater => {
                 println!("Too big!");
-                match previous_guess_direction {
-                    LESS => println!("Ok nice, now you know some boundaries"),
-                    GREATER => println!("Still going bigger, eh?"),
-                    _ => println!("Try again :)"),
-                };
-                previous_guess_direction = GREATER
+                if largest_guess.is_none() {
+                    largest_guess = Some(guess);
+                } else {
+                    if largest_guess.expect("There was no prev guess") > guess {
+                        largest_guess = Some(guess);
+                    } else {
+                        println!("Why did you waste your guess for nothing? That's stupid.");
+                    }
+                }
             }
             Ordering::Equal => {
                 println!("You win with {} guesses!", guesses);
                 break;
             }
         }
+
+        println!(
+            "You know the number is between {} - {}",
+            smallest_guess.unwrap_or(1),
+            largest_guess.unwrap_or(100)
+        );
     }
 }
