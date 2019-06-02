39745de22b0c3e2c0c05a8539e6c681252c7ce82
Vesa Laakso <laakso.vesa@gmail.com>
2019-06-02 16:05:45 +0300

    Exercise: Convert strings to pig latin
    Second exercise from https://doc.rust-lang.org/book/ch08-03-hash-maps.html#summary

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,33 @@
-use std::io;
 fn main() {
-    let mut raw_list = String::new();
-    io::stdin()
-        .read_line(&mut raw_list)
-        .expect("Failed to read line!");
+    let mut strings = vec![
+        "first".to_string(),
+        "apple".to_string(),
+        "fooboobaa".to_string(),
+        "orange".to_string(),
+        // The next would panic as we're being lazy and using .unwrap(),
+        // assuming every string always has a first character.
+        // "".to_string(),
+    ];
 
-    let mut ints: Vec<i32> = Vec::new();
+    let consonants = [
+        'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w',
+        'x', 'z',
+    ];
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
 
-    for part in raw_list.split(",") {
-        let parsed: i32 = match part.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Only integers, please!");
-                break;
+    for s in &mut strings {
+        let mut chars = s.chars();
+        let first_char = chars.next().unwrap();
+        if consonants.contains(&first_char) {
+            let mut new_s = String::new();
+            for c in chars {
+                new_s.push(c);
             }
-        };
-        ints.push(parsed);
-    }
-
-    println!("{:#?}", ints);
-
-    let mean = calc_average(&ints);
-    let median = calc_median(&ints);
-    let mode = calc_mode(&ints);
-    println!("mean: {:.2}", mean);
-    println!("median: {}", median);
-    println!("mode: {}", mode);
-}
-
-fn calc_average(ints: &Vec<i32>) -> f32 {
-    let mut sum: i32 = 0;
-    for i in ints {
-        sum += i;
-    }
-    let len = ints.len() as f32;
-    (sum as f32) / len
-}
-
-fn calc_median(ints: &Vec<i32>) -> i32 {
-    let mut sorted = ints.clone();
-    sorted.sort();
-    let middle_pos = sorted.len() / 2;
-    sorted[middle_pos]
-}
-
-fn calc_mode(ints: &Vec<i32>) -> i32 {
-    use std::collections::HashMap;
-
-    let mut seen_counts: HashMap<i32, u32> = HashMap::new();
-
-    for &i in ints {
-        let val = seen_counts.entry(i).or_insert(0);
-        *val += 1;
-    }
-
-    let mut largest_count = 0;
-    let mut value_seen = 0;
-
-    for (key, value) in seen_counts {
-        if value > largest_count {
-            value_seen = key;
-            largest_count = value;
+            *s = format!("{}-{}ay", new_s, first_char);
+        } else if vowels.contains(&first_char) {
+            s.push_str("-hay");
         }
     }
 
-    value_seen
+    println!("strings: {:#?}", strings);
 }
