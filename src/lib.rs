b6cdb846503397a54261463b03a4d2855a79f046
Vesa Laakso <laakso.vesa@gmail.com>
2019-06-02 14:04:38 +0300

    File structuring

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+mod front_of_house;
 
-use front_of_house::hosting;
+pub use front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
